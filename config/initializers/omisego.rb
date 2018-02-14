887c76945d40e6f4831ab024a320f7c86228cd80
Thibault <thibault@omise.co>
2018-02-14 11:59:08 +0700

    Add Rails logger for SDK requests

--- config/initializers/omisego.rb
+++ config/initializers/omisego.rb
@@ -2,4 +2,5 @@ OmiseGO.configure do |config|
   config.access_key = ENV['OMISEGO_ACCESS_KEY']
   config.secret_key = ENV['OMISEGO_SECRET_KEY']
   config.base_url   = ENV['OMISEGO_WALLET_URL']
+  config.logger     = Rails.logger
 end
