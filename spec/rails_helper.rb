cd55a06c953df248574478c5f672b2380489e90f
Thibault <thibault@omise.co>
2018-06-20 15:45:11 +0700

    Handle DatabaseCleaner safeguards

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -15,6 +15,9 @@ Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 ActiveRecord::Migration.maintain_test_schema!
 
+DatabaseCleaner.allow_production = true
+DatabaseCleaner.allow_remote_database_url = true
+
 RSpec.configure do |config|
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
