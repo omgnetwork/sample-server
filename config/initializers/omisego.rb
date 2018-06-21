0cd301ace77630fa9d973928e53e88e904c9354d
Thibault <thibault@omise.co>
2018-06-21 16:56:12 +0700

    Remove account_id check

--- config/initializers/omisego.rb
+++ config/initializers/omisego.rb
@@ -1,5 +1,3 @@
-raise "OmiseGO eWallet master 'ACCOUNT_ID' ENV not set" unless ENV['ACCOUNT_ID']
-
 OmiseGO.configure do |config|
   config.access_key = ENV['OMISEGO_ACCESS_KEY']
   config.secret_key = ENV['OMISEGO_SECRET_KEY']
